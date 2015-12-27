38e9d41b331c5a05996c595c1601c555173a000e
Frank van Wijk <fvanwijk@xebia.com>
2015-12-27 12:41:58 +0100

    Solved day 8 part 2 by counting chars that will be escaped

--- 8.js
+++ 8.js
@@ -1,10 +1,12 @@
 require('fs').readFile('8.txt', 'utf8', function (err, str) {
   const res = str.split('\n').reduce(function (count, word) {
     eval('var a = ' + word);
-    count += word.length - a.length;
-    //console.log(word, word.length, new String(word).length);
+    count.words += word.length;
+    count.mem += a.length;
+    count.encoded += word.match(/["\\]/g).length + 2;
     return count;
-  }, 0);
+  }, { words: 0, mem: 0, encoded: 0 });
 
-  console.log('Diff between string and memory representation: ' + res); // 1342
+  console.log('Diff between string and memory representation: ' + (res.words - res.mem)); // 1342
+  console.log('Diff between string and encoded representation: ' + res.encoded); // 2074
 });
