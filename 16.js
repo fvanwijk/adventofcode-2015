14259714a71308965f9f16f05bb7bbfa2110109d
Frank van Wijk <fvanwijk@xebia.com>
2016-01-03 14:54:09 +0100

    Solved day 16b and cleaned up all code plus make ES6 friendly

--- 16.js
+++ 16.js
@@ -1,40 +1,50 @@
-require('fs').readFile('16.txt', 'utf8', function (err, str) {
-  const props = {
-    children: 3,
-    cats: 7,
-    samoyeds: 2,
-    pomeranians: 3,
-    akitas: 0,
-    vizslas: 0,
-    goldfish: 5,
-    trees: 3,
-    cars: 2,
-    perfumes: 1
-  };
+require('fs').readFile('16.txt', 'utf8', (err, str) => {
+  function checkProps(isSecondAssignment) {
+    return props.every(propSet => checkProp.apply(this, propSet.concat(isSecondAssignment)));
+  }
 
-  const aunts = str.split('\n').map(function (aunt) {
-    var regex = /Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)/;
-    const matches = aunt.match(regex);
-    var auntDetails = {};
-    auntDetails[matches[2]] = +matches[3];
-    auntDetails[matches[4]] = +matches[5];
-    auntDetails[matches[6]] = +matches[7];
-    return auntDetails;
+  function checkProp(prop, op, val, isSecondAssignment) {
+    op = isSecondAssignment ? op : '===';
+
+    return !aunt.hasOwnProperty(prop) || {
+      '===': aunt[prop] === val,
+      '>': aunt[prop] > val,
+      '<': aunt[prop] < val
+    }[op];
+  }
+
+  const aunts = str.split('\n').map(aunt => {
+    const matches = aunt.match(/Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)/);
+    return {
+      name: matches[1],
+      [matches[2]]: +matches[3],
+      [matches[4]]: +matches[5],
+      [matches[6]]: +matches[7]
+    };
   });
 
-  for (var i = 0; i < aunts.length; i++) {
-    var isCorrectAunt = true;
-    for (var prop in aunts[i]) {
-      //console.log('compare ' + prop + ' ' + aunts[i][prop] + ' with ' + props[prop]);
-      if (aunts[i][prop] !== props[prop]) {
-        isCorrectAunt = false;
-        break;
-      }
+  const props = [
+    ['cats', '>', 7],
+    ['trees', '>', 3],
+    ['goldfish', '<', 5],
+    ['pomeranians', '<', 3],
+    ['children', '===', 3],
+    ['samoyeds', '===', 2],
+    ['akitas', '===', 0],
+    ['vizslas', '===', 0],
+    ['cars', '===', 2],
+    ['perfumes', '===', 0]
+  ];
+
+  for (var aunt of aunts) {
+    if (checkProps()) {
+      console.log('1. The correct aunt Sue is number ' + aunt.name, aunt);
+      // 1. The correct aunt Sue is number 40 { name: '40', vizslas: 0, cats: 7, akitas: 0 }
     }
 
-    if (isCorrectAunt) {
-      console.log('The correct aunt Sue is number ' + (i+1), aunts[i]);
-      break;
+    if (checkProps(true)) {
+      console.log('2. The correct aunt Sue is number ' + aunt.name, aunt);
+      // 2. The correct aunt Sue is number 241 { name: '241', cars: 2, pomeranians: 1, samoyeds: 2 }
     }
   }
 });
